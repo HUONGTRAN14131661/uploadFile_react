67d5a1b2ab18c4f4807a0a623a34b8dd162500d3
tranthithuhuong14131661 <tranthithuhuong14131661@gmail.com>
2021-12-20 23:26:28 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React from "react";
+// import React from "react";
 import { useState } from "react"
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 
-function App() {
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
+import React from 'react';
+import {useDropzone} from 'react-dropzone';
+// import { useDropzone } from 'react-dropzone'
 
-  const changeHandler = (e) => {
-    console.log(e.target.files[0]);
-      const a = setSelectedFile(e.target.files[0]);
-      console.log('a', a);
-      setIsFilePicked(true);
-  }
+function App(props) {
+
+  const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone({
+    // Disable click and keydown behavior
+    noClick: true,
+    noKeyboard: true
+  });
+
+  const files = acceptedFiles.map(file => (
+      <ul key={file.path}> <strong>Detail:</strong>
+        <li>File name: {file.name}</li>
+        <li>File path: {file.path}</li>
+        <li>File type: {file.type}</li>
+        <li>Size in bytes: {file.size}</li>
+        <li>
+          Last modified date:{' '}
+          {file.lastModifiedDate.toLocaleDateString()}
+        </li>
+      </ul>
+  ));
+  console.log(1, getRootProps)
+  console.log(2, getInputProps)
+  console.log(3, open)
+  console.log(4, acceptedFiles.map(file => ( file.name) ))
+
   const handleSubmission = () => {
       const formData = new FormData()
     
-      const path = selectedFile.file.name.substring(0, selectedFile.file.name.lastIndexOf('/'))
-    formData.append('parent_folders', `/abc-backend${path}/`)
-    formData.append('need_transcode', 'true')
-    formData.append('name', 'upload')
-    formData.append('shared_id', '2811270996997413898_441ff609-0a21-404d-af88-e72ccda60bcb')
-    formData.append('upload', selectedFile)
+      const path = acceptedFiles.map(file => (
+        file.name.substring(0,file.name.lastIndexOf('/'))
+       
+      ))
+      console.log(path)
+
+      // const path = selectedFile.file.name.substring(0, selectedFile.file.name.lastIndexOf('/'))
+      formData.append('parent_folders', `/abc-backend${path}/`)
+      formData.append('need_transcode', 'true')
+      formData.append('name', 'upload')
+      formData.append('shared_id', '2811270996997413898_441ff609-0a21-404d-af88-e72ccda60bcb')
+      formData.append('upload', acceptedFiles)
     
-      console.log(1)
-      console.log(selectedFile)
       fetch(
         '<change your upload file url here>',
         {
@@ -47,25 +70,20 @@ function App() {
 
   return (
     <div className="container">
-      <div className="row-upload">
-        <input className="custom-file-input" type="file" name="file" onChange = {changeHandler}/>
+      <div className="container">
+        <div {...getRootProps({className: 'dropzone'})}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here</p>
+          <button type="button" onClick={open}>
+            Open File Dialog
+          </button>
+        </div>
+        <aside>
+          <div>{files}</div>
+        </aside>
       </div>
-     
-      {isFilePicked ? (
-        <div className="content-has">
-        <p>FileName: {selectedFile.name}</p>
-        <p>Filetype: {selectedFile.type}</p>
-        <p>Size in bytes: {selectedFile.size}</p>
-        <p>
-          lastModifiedDate:{' '}
-          {selectedFile.lastModifiedDate.toLocaleDateString()}
-        </p>
-      </div>
-      ) : (
-        <p className="content-none">Select a file to show details</p>
-      )
-      }
-      <div>
+ 
+      <div className="container-btn">
         <button className="btn-submit" onClick={handleSubmission}>Submit</button>
       </div>
   </div>
